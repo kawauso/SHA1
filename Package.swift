68346f15ac8e3e98399bae0160c5bc28c554166e
Tanner Nelson <tanner@bluebite.com>
2016-04-05 15:46:30 -0400

    update to cryptoessentials 0.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "SHA1",
     dependencies: [
-                      .Package(url: "https://github.com/CryptoKitten/CryptoEssentials.git", majorVersion: 0, minor: 1),
-                      .Package(url: "https://github.com/SwiftX/C7.git", majorVersion: 0, minor: 1)
-                      ]
+        .Package(url: "https://github.com/CryptoKitten/CryptoEssentials.git", majorVersion: 0, minor: 2),
+    ]
 )
